17a9defda6f9d6e631f277f8b65785d35e1ecd95
MohamedFazilJ <22it103@skcet.ac.in>
2024-07-24 09:11:45 +0530

    fazil

--- library-management-react/src/components/Admin.js
+++ library-management-react/src/components/Admin.js
@@ -1,10 +1,22 @@
 import '../cssfolder/Admin.css';
 
-export default function Admin()
-{
-    return(
-        <div>
+export default function Admin() {
+    return (
+        <div className="admin-body">
+        <div className="admin-container">
             <h1>Admin Login</h1>
+            <form className="admin-form">
+                <div className="form-group">
+                    <label htmlFor="email">Email:</label>
+                    <input type="email" id="email" name="email" required />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="password">Password:</label>
+                    <input type="password" id="password" name="password" required />
+                </div>
+                <button type="submit" className="login-button">Login</button>
+            </form>
+        </div>
         </div>
     );
 }
