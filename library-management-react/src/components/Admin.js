a10bf0892d4a91f3d209edb84519868fa1ad8dbc
MohamedFazilJ <22it103@skcet.ac.in>
2024-07-29 14:48:57 +0530

    adminnavbar change

--- library-management-react/src/components/Admin.js
+++ library-management-react/src/components/Admin.js
@@ -10,7 +10,7 @@ export default function Admin() {
     const [password, setPassword] = useState('');
     const [error, setError] = useState('');
     const navigate = useNavigate();
-    const { setIsAdminLoggedIn} = useContext(AuthContext);
+    const { setIsAdminLoggedIn } = useContext(AuthContext);
 
     const handleSubmit = async (event) => {
         event.preventDefault();
@@ -21,15 +21,13 @@ export default function Admin() {
                 password
             });
 
-            if(response.data===true)
-            {
-                setError("Login Successfull");
+            if (response.data === true) {
+                setError("Login Successful");
                 setTimeout(() => {
                     setIsAdminLoggedIn(true);
                     navigate("/adminHome");
                 }, 1000);
-            }
-            else{
+            } else {
                 setError('Invalid Credentials');
             }
 
@@ -41,33 +39,36 @@ export default function Admin() {
     return (
         <div className="admin-body">
             <div className="admin-container">
-                <h1>Admin Login</h1>
-                <form className="admin-form" onSubmit={handleSubmit}>
-                    <div className="admin-form-group">
-                        <label htmlFor="email">Email:</label>
-                        <input 
-                            type="email" 
-                            id="email" 
-                            name="email" 
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                            required 
-                        />
-                    </div>
-                    <div className="admin-form-group">
-                        <label htmlFor="password">Password:</label>
-                        <input 
-                            type="password" 
-                            id="password" 
-                            name="password" 
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                            required 
-                        />
-                    </div>
-                    {error && <p className="admin-error-message">{error}</p>}
-                    <button type="submit" className="login-button">Login</button>
-                </form>
+                <div className="admin-image"></div>
+                <div className="admin-login">
+                    <h1>Admin Login</h1>
+                    <form className="admin-form" onSubmit={handleSubmit}>
+                        <div className="admin-form-group">
+                            <label htmlFor="email">Email:</label>
+                            <input 
+                                type="email" 
+                                id="email" 
+                                name="email" 
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                                required 
+                            />
+                        </div>
+                        <div className="admin-form-group">
+                            <label htmlFor="password">Password:</label>
+                            <input 
+                                type="password" 
+                                id="password" 
+                                name="password" 
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                required 
+                            />
+                        </div>
+                        {error && <p className="admin-error-message">{error}</p>}
+                        <button type="submit" className="login-button">Login</button>
+                    </form>
+                </div>
             </div>
         </div>
     );
