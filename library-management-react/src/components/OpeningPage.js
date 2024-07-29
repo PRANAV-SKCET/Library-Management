cc0dbbcfe3ea23d6c6eb9a00ab348f51a0979045
PRANAV-SKCET <pranavarulprakash@gmail.com>
2024-07-29 10:19:27 +0530

    commit

--- library-management-react/src/components/OpeningPage.js
+++ library-management-react/src/components/OpeningPage.js
@@ -4,13 +4,13 @@ import '../cssfolder/OpeningPage.css';
 export default function OpeningPage(){
     return(
         <div className='OpeningPage-container'>
-            <h1 className='OpeningPage-title'>SKCET Library</h1>
+            <h1 className='OpeningPage-title'>Book Sphere Library</h1>
             <div className='OpeningPage-links'>
                 <Link to="/admin" className='OpeningPage-link'>Admin</Link>
                 <Link to="/searchBooks" className='OpeningPage-link'>Member</Link>
             </div>
             <footer className='OpeningPage-footer'>
-                <p>© 2024 SKCET Library. All rights reserved.</p>
+                <p>© 2024 Book sphere Library. All rights reserved.</p>
                 <div className='OpeningPage-social-links'>
                     <a href='#' className='OpeningPage-social-link'>Facebook</a>
                     <a href='#' className='OpeningPage-social-link'>Twitter</a>
